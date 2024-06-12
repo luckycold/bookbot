7c75c982ffc2771308fe051125e0037237a74384
Luke <git@1al.cc>
2024-06-12 07:55:00 -0500

    Finished bookbot

--- main.py
+++ main.py
@@ -4,17 +4,22 @@ def main():
 
 def print_report(book_location):
     text = get_book_text(book_location)
-    print(f"--- Begin report of {book} ---")
+    print(f"--- Begin report of {book_location} ---")
     print(f"{get_word_count(text)} words found in the document")
     print("\n")
-    for letter in
+    chars_dict = get_character_counts(text)
+    chars_list = convert_character_counts(chars_dict)
+    for char in chars_list:
+        print(f"The '{char["char"]}' character was found {char["freq"]} times")
+    print("--- End report ---")
+
 
 def convert_character_counts(chars_dict: dict):
     chars_list = []
     for key in chars_dict.keys():
         if key.isalpha():
             chars_list.append({"char": key, "freq": chars_dict[key]})
-    chars_list.sort(key=lambda dict: dict["freq"])
+    chars_list.sort(reverse=True ,key=lambda dict: dict["freq"])
     return chars_list
 
 def get_book_text(book):
